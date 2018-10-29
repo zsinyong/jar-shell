323ec72db99b6cf243368e777aecb196a1b6e09a
qyzhu6 <13215658013@163.com>
2018-10-29 21:35:23 +0800

    First commit

---
+++ jar-shell-monitor/src/main/java/me/tsinyong/jar/shell/monitor/JarShellMonitorApplication.java
@@ -0,0 +1,12 @@
+package me.tsinyong.jar.shell.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarShellMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JarShellMonitorApplication.class, args);
+    }
+}
---
+++ jar-shell-monitor/src/test/java/me/tsinyong/jar/shell/monitor/JarShellMonitorApplicationTests.java
@@ -0,0 +1,16 @@
+package me.tsinyong.jar.shell.monitor;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JarShellMonitorApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
